8613957be14a4c635d94353db2281524cbb8232f
nikbagramyan <nikbagramyan1@gmail.com>
2026-05-05 12:49:17 -0700

    Fix google callback URL for production

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(express.urlencoded({ extended: true }));
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback"
+  callbackURL: "https://newyeartracker.onrender.com/auth/google/callback"
 }, (accessToken, refreshToken, profile, done) => {
 
   const email = profile.emails && profile.emails[0] && profile.emails[0].value;
